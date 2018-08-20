7ca884fac755f9355959d3e60bcfdadf19c0ff19
CodyZhuo <33078893+CodyZhuo@users.noreply.github.com>
2018-08-20 20:47:38 +0800

    Overriding Context

--- worktrend/models.py
+++ worktrend/models.py
@@ -9,9 +9,12 @@ from wagtail.search import index
 class WorktrendIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        worktrendpage = self.get_children().live().order_by('-first_published_at')
+        context['worktrendpage'] = worktrendpage
+        return context
 
 
 class WorktrendPage(Page):
